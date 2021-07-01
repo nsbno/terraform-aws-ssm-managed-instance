8a4f1e10a98be220f21dcb613898988e62d808f1
stekern <stekern@users.noreply.github.com>
2021-07-01 16:28:51 +0200

    Add names of SSM parameters as outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "activation_code_ssm_parameter_arn" {
   value = aws_ssm_parameter.ssm_activation_code.arn
 }
 
+output "activation_id_ssm_parameter_name" {
+  value = aws_ssm_parameter.ssm_activation_id.name
+}
+
+output "activation_code_ssm_parameter_name" {
+  value = aws_ssm_parameter.ssm_activation_code.name
+}
+
 output "instance_name" {
   value = local.instance_name
 }

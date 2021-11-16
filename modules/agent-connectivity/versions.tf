5f5b77a109f6d4245ff37abde09ed081c09bd318
Fredrik Hov <37329363+frehov@users.noreply.github.com>
2021-11-16 21:35:23 +0100

    Update terraform version and unify terraform/aws version across modules

--- modules/agent-connectivity/versions.tf
+++ modules/agent-connectivity/versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.46.0"
+      version = ">= 3.13.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = ">= 1.3.0"
+      version = ">= 2.2.0"
     }
   }
 }
--- modules/cloudwatch-agent/versions.tf
+++ modules/cloudwatch-agent/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.46.0"
+      version = ">= 3.13.0"
     }
   }
 }
